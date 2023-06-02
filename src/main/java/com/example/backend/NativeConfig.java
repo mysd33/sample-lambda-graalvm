fefcf361aab12d6899a4b0eb1867dbaee29a3b4c
mysd33 <mysd.33@gmail.com>
2023-06-02 20:39:14 +0900

    GraalVMでのリフレクション対応

---
+++ src/main/java/com/example/backend/NativeConfig.java
@@ -0,0 +1,27 @@
+package com.example.backend;
+
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.backend.app.handler.todo.TodoResource;
+import com.example.backend.app.handler.user.UserResource;
+import com.example.backend.domain.model.Todo;
+import com.example.backend.domain.model.User;
+import com.example.backend.infra.repository.TodoTableItem;
+import com.example.backend.infra.repository.UserTableItem;
+
+/**
+ * GraalVM向けにリフレクションのヒントを与えるため設定クラス
+ *
+ */
+@Configuration
+@RegisterReflectionForBinding({
+    UserResource.class,
+    TodoResource.class,
+    User.class,
+    Todo.class,
+    UserTableItem.class,
+    TodoTableItem.class    
+})
+public class NativeConfig {    
+}
